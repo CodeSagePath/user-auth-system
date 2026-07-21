e026f42b4a695c504f0e8f87e1b5e76deeabb468
CodeSagePath <rishigarg185@gmail.com>
2026-07-21 20:30:28 +0530

    Routing setup:   - 1. Wrapped the application with BrowserRouter in the root component.   - 2. Setup Routes for Login, Dashboard & Profile Pages.   - 3. Created placeholders for each of these components.   - 4. Esured routes are rendering correct component.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { Link, Routes, Route } from "react-router-dom";
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
+import Profile from './components/Profile';
 
 function App() {
   return (
     <div className="App">
       <h1>User Authentication System</h1>
+
+      <nav>
+        <ul>
+          <li><Link to="/login">Login</Link></li>
+          <li><Link to="/Dashboard">Dashboard</Link></li>
+          <li><Link to="/profile">Profile</Link></li>
+          <li><button>Logout</button></li>
+        </ul>
+      </nav>
+
+
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
     </div>
   );
 }
