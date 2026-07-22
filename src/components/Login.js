76a4887e58d6eb4610db2669b8aa860b9d307a63
CodeSagePath <rishigarg185@gmail.com>
2026-07-22 20:24:34 +0530

    Updates:   - Added auth reducer default export and handled login/logout state updates   - Ensure reducer throws on invalid action types

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,52 @@
+import { useState, useContext } from "react";
+import { AuthContext } from "../context/Auth";
+
+import users from "../temp_data/users_data.json";
+
 export default function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const { handleLogin } = useContext(AuthContext);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const formData = {
+      username, // ES6 concise properties
+      password, // ES6 concise properties
+    };
+
+    const user = users.find((user) => user.username === formData.username && user.password === formData.password);
+
+    if(user) {
+      handleLogin(user);
+      setErrorMessage('');
+    } else {
+      setErrorMessage('Invalid Credentials.');
+    }
+  };
+
   return (
     <div>
-      <h1> Login Component </h1>
+      <h1> Login Form </h1>
+
+      {errorMessage && <p className="error">{errorMessage}</p>}
+
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Enter username" />
+        </div>
+
+        <div className="form-group">
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Enter password" />
+        </div>
+
+        <div className="form-group">
+          <input type="submit" value="Login" />
+        </div>
+      </form>
     </div>
   );
 }
--- src/context/Auth.js
+++ src/context/Auth.js
@@ -1,4 +1,6 @@
 import { createContext, useReducer } from "react";
+import { useNavigate } from "react-router-dom";
+
 import reducer from "../reducers/authReducer.js";
 
 // Auth Context
@@ -12,8 +14,16 @@ const initialState = {
 // Auth Provider
 export function AuthProvider(props) {
   const [state, dispatch] = useReducer(reducer, initialState);
+
+  const navigate = useNavigate();
+
+  const handleLogin = user => {
+    dispatch({ type: 'LOGIN', payload: user});
+    navigate("/dashboard");
+  };
+
   return (
-    <AuthContext value={{ ...state, dispatch }}>
+    <AuthContext value={{ ...state, dispatch, handleLogin }}>
       { props.children }
     </AuthContext>
   );
