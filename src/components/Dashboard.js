bcae7150417ce14c64e1797152aa27202cc60b4a
CodeSagePath <rishigarg185@gmail.com>
2026-07-23 09:48:19 +0530

    Updates:   - Add useContext/AuthContext to Dashboard; show loading state and welcome user   - Add useContext/AuthContext to Profile; show loading state and render ID, username, and masked password   - Update Profile header to "UserProfile"

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,7 +1,17 @@
+import { useContext } from "react";
+import { AuthContext } from "../context/Auth";
+
 export default function Dashboard() {
+  const { user } = useContext(AuthContext);
+
+  if(!user) {
+    return <p>Loading...</p>
+  }
+
   return (
     <div>
       <h1> Dashboard Component </h1>
+      <p>Welcome {user.username}.</p>
     </div>
   );
 }
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,7 +1,19 @@
+import { useContext } from "react";
+import { AuthContext } from "../context/Auth";
+
 export default function Profile() {
+  const { user } = useContext(AuthContext);
+
+  if(!user) {
+    return <p>Loading...</p>
+  }
+
   return (
     <div>
-      <h1> Profile Component </h1>
+      <h2> UserProfile </h2>
+      <p>ID - {user.id}</p>
+      <p>Username - {user.username}</p>
+      <p>Password - {'*'.repeat(user.password.length)}</p>
     </div>
   );
 }
